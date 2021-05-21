e9e27294d310d1ee46b270d7ccb0cd0eb0edfb5a
Tejas Ladhani <67834407+Tejas-Ladhani@users.noreply.github.com>
2021-05-21 19:01:53 +0530

    material UI added

--- src/containers/create-post/index.js
+++ src/containers/create-post/index.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { SignInBtn } from '../../components'
+import { UserContext } from '../../contexts/user'
 import './style.css'
 export default function CreatePost() {
+    const [user, setUser] = useContext(UserContext);
+    const [caption, setCaption] = useState("")
+    const handleChange = () => {
+
+    }
+
     return (
         <div className="createPost">
-            <SignInBtn/>
-            <p style={{paddingLeft:'10px'}}> to post and Create </p>
+            {   user ?
+                <div className="createPost_loggedIn">
+                    <p>Create Post</p>
+                    <div className="createPost_loggedInCenter">
+                        <textarea className="createPost_textarea" rows="3" value={caption}
+                            onChange={(e) => { setCaption(e.target.value) }}
+                        >
+                        </textarea>
+                    </div>
+                    <div>
+                        <input type="file" accept="image/*" onChange={handleChange} />
+                    </div>
+                </div>
+                : <>
+                    <SignInBtn />
+                    <p style={{ paddingLeft: '10px' }}> to post and Create </p>
+                </>
+            }
         </div>
     )
 }
