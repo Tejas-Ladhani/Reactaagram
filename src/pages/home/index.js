ad6350ba236670c383025577b3a858af5ed679a2
Tejas Ladhani <67834407+Tejas-Ladhani@users.noreply.github.com>
2021-06-03 20:23:44 +0530

    minor bug solved

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import { CreatePost, Feed, Navbar } from '../../containers'
+import { UserContext } from '../../contexts/user';
 import './style.css'
 export default function Home() {
+    const [user, setUser] = useContext(UserContext);
     return (
         <div className="home">
              <Navbar/>
              <CreatePost/>
-             <Feed/>
+             {user?<Feed/>:<div className="home_feed_fallback">[ You need to sign in to see the feed ! ]</div>}
+             
         </div>
     )
 }
